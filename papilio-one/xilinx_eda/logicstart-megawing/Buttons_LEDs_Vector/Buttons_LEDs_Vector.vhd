a8c857c3ee4c0d7efbb1f92af7fdc624ee3e517b
Patrick Vogt <mail@patrickvogt.net>
2013-09-10 23:30:21 +0200

    pavo: adjusted project for logic start megawing

--- papilio-one/xilinx_eda/logicstart-megawing/Buttons_LEDs_Vector/Buttons_LEDs_Vector.vhd
+++ papilio-one/xilinx_eda/logicstart-megawing/Buttons_LEDs_Vector/Buttons_LEDs_Vector.vhd
@@ -2,15 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Buttons_LEDs_Vector is
-    Port ( DPAD : in STD_LOGIC_VECTOR(3 downto 0);
-           LED  : out STD_LOGIC_VECTOR(3 downto 0) 
+    Port ( Switch : in STD_LOGIC_VECTOR(7 downto 0);
+           LED  : out STD_LOGIC_VECTOR(7 downto 0) 
 			);
 end Buttons_LEDs_Vector;
 
 architecture Behavioral of Buttons_LEDs_Vector is
 
 begin
-		--LED <= DPAD;
-		LED <= "1111";
+		LED <= Switch;
+		--LED <= "1111";
 end Behavioral;
 
